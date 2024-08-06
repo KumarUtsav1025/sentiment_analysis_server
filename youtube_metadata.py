d0575af414942db1141e822bfafddb8f2020d70d
Kumar Utsav <utsavkumar.9749@gmail.com>
2024-08-07 01:43:41 +0530

    fixed url

--- youtube_metadata.py
+++ youtube_metadata.py
@@ -7,19 +7,22 @@ def get_youtube_metadata(video_id):
     load_dotenv()
     rapidapi_key = os.getenv("RAPIDAPI_KEY")
 
-    url = "https://youtube-metadata-extractor.p.rapidapi.com/get_youtube_video_title_description"
+    url = "https://youtube-video-information1.p.rapidapi.com/api/youtube"
 
-    querystring = {"url": f"https://youtu.be/{video_id}"}
+    querystring = {"video_id":video_id}
     headers = {
         "x-rapidapi-key": f"{rapidapi_key}",
-        "x-rapidapi-host": "youtube-metadata-extractor.p.rapidapi.com"
+        "x-rapidapi-host": "youtube-video-information1.p.rapidapi.com"
     }
 
     response = requests.get(url, headers=headers, params=querystring)
     data = response.json()
-    video_name = data['data']['title']
+    print(data)
+    video_name = data['title']
     video_name = video_name[0:25]
-    image_url = data['data']['thumbnails'][1]['url']
+    image_url = data['thumbnail']
 
     print(video_name, image_url)
     return video_name, image_url
+
+get_youtube_metadata("A1vEZhwH8y8")
